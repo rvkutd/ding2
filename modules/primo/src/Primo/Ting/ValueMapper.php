63cb21a77191061339f08a3e78c7b72f4f445abc
Kasper Garnaes <kasper.garnaes@gmail.com>
2017-12-11 15:19:52 +0100

    Use administrative interface for handling genres
    - Create text area for mapping in administration
    - Use variable to perform actual mapping of Genres
    - Check for genre mapping in hook_requirements
    - Initialize site with genre map
    
    BBS-160

--- modules/primo/src/Primo/Ting/ValueMapper.php
+++ modules/primo/src/Primo/Ting/ValueMapper.php
@@ -99,10 +99,11 @@ class ValueMapper {
    *   The Primo code.
    *
    * @return string
-   *   The translated code or the input code if it could not be mapped.
+   *   The mapped code or the input code if it could not be mapped.
    */
   public static function mapGenreFromCode($code) {
-    return t($code, [], ['context' => static::T_CONTEXT_GENRE]);
+    $map = variable_get('primo_genre_map', []);
+    return (!empty($map[$code])) ? $map[$code] : $code;
   }
 
   /**
@@ -111,7 +112,8 @@ class ValueMapper {
    * @return false|int|string
    */
   public static function mapGenreToCode($genre) {
-    return static::reverseTranslate($genre, static::T_CONTEXT_GENRE);
+    $map = array_flip(variable_get('primo_genre_map', []));
+    return (!empty($map[$genre])) ? $map[$genre] : $genre;
   }
 
   /**
