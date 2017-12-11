5f167dc8ebabca1ce44412b21aacdcfb36d27d44
Mathias Larsen <mathias@reload.dk>
2017-12-11 11:42:40 +0100

    Set classifications. BBS-189

--- modules/primo/src/Primo/Ting/ObjectMapper.php
+++ modules/primo/src/Primo/Ting/ObjectMapper.php
@@ -82,15 +82,13 @@ class ObjectMapper {
     // TODO BBS-SAL: Implement getURI() method.
     $object->setType($this->document->getType());
     $object->setYear($this->document->getYear());
-
+    $object->setClassifications($this->document->getLocalSearchField(12));
 
     // Below are parts of the TingObjectInterface which the Primo modules
     // currently do not support.
     // Note that this does not necessarily mean that the information is not
     // available from Primo. It is just not implemented at the moment. Please
     // check each setter-invocation for any additional information.
-    // We do not support classification.
-    $object->setClassifications(FALSE);
     // The contents of this field is partly duplicated by getExtent().
     $object->setFormat(FALSE);
     $object->setIsPartOf([]);
